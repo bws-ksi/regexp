2a3f25c05137d7a52c744827a7b8a4c798a4c28a
bws.ksi <bws.ksi@gmail.com>
2022-04-20 23:09:06 +0300

    edit script, but don`t work

--- script.js
+++ script.js
@@ -10,10 +10,27 @@ const arrGroups = [
                   'FE2008-52',
                   'Fm2008-23',
                   'FM2022-1',
+                  'FD2022-5',
+                  'FD2022-5',
+                  'FD2022-5',
+                  'FD2022-5',
+                  'FD2022-5',
+                  'FD2022-5',
+                  'FD2022-5',
+                  'FD2022-5',
 ];
-const groupChek = /(\W|^)(FD|FM|FE|fd|fm|fe)(20)\d\d(-\d+)?/g;
+// const groupChek = /(\W|^)(FD|FM|FE|fd|fm|fe)(20)\d\d(-\d+)?/g;
+const groupChek = /^(F[DME]|f[dme])20\d\d(-\d+)?$/g;
 
-for (let i = 0; i < arrGroups.length; i++) {
-  console.log(groupChek.test(arrGroups[i]) ? arrGroups[i]+' - Success' : arrGroups[i]+" - Error");
-  // console.log('==============')
+function validationGroups () {
+  for (let i = 0; i < arrGroups.length; i++) {
+    if (groupChek.test(arrGroups[i])) {
+      console.log(arrGroups[i]+ ' - Success' );   
+    } else {
+      console.log(arrGroups[i] + ' - Error');
+    }
+    // console.log(groupChek.test(arrGroups[i]) ? arrGroups[i]+ ' - Success' : arrGroups[i] + ' - Error');
+  }
 }
+
+validationGroups ();
